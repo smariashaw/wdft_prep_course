642ddc2ae9d084271648b2059c03544d94bf2208
smariashaw <smariashaw@gmail.com>
2017-11-22 09:39:36 -0500

    final submission

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -10,19 +10,24 @@ console.log(numbers);
 Exercise #2
 Using the loop provided for you below, use the array push method to insert the numbers 1 to 10 into the array.
 */
-var numbers=[10];
+var numberArray=[];
+
 for(var i=0;i<10;i++){
-    numbers[i]=
-    //use the array push() method here.
+    numberArray.push(i+1);
 }
+console.log(numberArray);
+
+    
+    //use the array push() method here.
+
 
 
 /*
 Exercise #3
 Test that you used the array push method correctly by console logging the first item in the array. Console log the last item in the array afterwards.
 */
-console.log();
-console.log();
+console.log(numberArray[0]);
+console.log(numberArray[9]);
 
 
 /*
@@ -30,6 +35,8 @@ Exercise #5
 Create an empty object and assign it to the variable car below.
 Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputting the "colour" property to the console. You should see the value "black" printed out.
 */
-var car;
-
+var car = {
+    color:'black'
+};
+console.log (car.color);
 
--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -1,21 +1,44 @@
 // Exercise #1
 // Write your solution below
 
+for (i=1;i<8;i++){
 
+    var output = '';
+    for (j=0;j<i;j++){
+        output += '#';
+        
+    }
+    
+    console.log(output);
+}
 /* 
 Write your explanation for Exercise #1 here
+I used a loop inside a loop to log the triangle to the console.
+The first loops shows I want it to run 7 times ( 7 lines)
+The second loop inside shows for each line of the first loop (i)
+1 will be added by the second loop (j)
+The second loop will end at the end of the first loop (or when shown to be false)
 
 */
 
 
 
-
-
 // Exercise #2
 // Write your solution below
-
+function isEven(number) {
+    if (number % 2 == 0) {
+        return 'The number is even!';
+    } else {
+        return 'The number is odd!';
+    }
+}
+isEven (44);
 
 /* 
 Write your explanation for Exercise #2 here
+Using the modulo operator we can determine if a number can be divided ( in this case)
+by 2. If the remainder is 0 ( or equal to) the number is even, if not the number is odd.
+We use an if...else statement to print the string that is true or false.
+I used the number 44, which is even so 'the number is even!' will print.
 
 */
